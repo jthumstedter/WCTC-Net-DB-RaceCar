d76fd083c5806157131e6697ae22138bb47165e8
jthumstedter <jthumstedter@gmail.com>
2024-03-14 18:36:35 -0500

    Implemented the Lightning driver and added the new driver and car to the Race Track program

---
+++ RaceTrack/Drivers/Lightning.cs
@@ -0,0 +1,22 @@
+﻿using RaceTrack.RaceTrack.Cars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    public class Lightning : Driver
+    {
+        public Lightning(RaceCar car) : base(car)
+        {
+            Name = "Lightning";
+            SkillLevel = 9;
+        }
+        public override void Drive()
+        {
+            Car.Accelerate(SkillLevel);
+        }
+    }
+}
