c1122515f3fd88c8400285d3acb4914c41edc7f7
jthumstedter <jthumstedter@gmail.com>
2024-03-14 18:29:26 -0500

    Added the corvette car into cars

---
+++ RaceTrack/Cars/Corvette.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    public class Corvette : RaceCar
+    {
+        public Corvette()
+        {
+            Name = "Corvette";
+            TopSpeed = 95;
+        }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} engine starts");
+        }
+    }
+}
